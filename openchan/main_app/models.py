dd36ab438b8a749b43efc8cd4e6ccd950634bd7e
Urvatov <urvatov@gmail.com>
2024-02-12 22:59:40 +0300

    +class Board

--- openchan/main_app/models.py
+++ openchan/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    tag = models.CharField(max_length = 64)
+    title = models.CharField(max_length = 24)
+    description = models.TextField(blank = True)
+    all_posts = models.IntegerField()
